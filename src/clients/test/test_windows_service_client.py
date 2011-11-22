c668e78d0460c83722085c7d812b17238e5b8ec4
Alec Munro <alec.munro@arc90.com>
2011-11-22 13:14:24 -0400

    Enhancing test coverage for WindowsServiceClient

--- src/clients/test/test_windows_service_client.py
+++ src/clients/test/test_windows_service_client.py
@@ -5,11 +5,28 @@ Created on 2011-11-21
 '''
 from unittest.case import TestCase
 import clients.windows_service_client as mut
+import mock
 
 SAMPLE_SERVICE = "Widgit factory"
+SAMPLE_MACHINE = "Grandma"
+SAMPLE_ADDR = mut.ADDR_TMPL.format(SAMPLE_MACHINE)
 
 class TestWindowsServiceClient(TestCase):
     
-    def test_start(self):
-        client = mut.WindowsServiceClient()
+    @mock.patch("subprocess.Popen")
+    def test_start(self, m_Popen):
+        """Start a service."""
+        client = mut.WindowsServiceClient(SAMPLE_MACHINE)
         client.start_service(service_name=SAMPLE_SERVICE)
+        m_Popen.assert_called_with([mut.SERVICE_CMD, SAMPLE_ADDR, mut.START_CMD, 
+                                    SAMPLE_SERVICE])
+        
+    
+    @mock.patch("subprocess.Popen")
+    def test_stop(self, m_Popen):
+        """Stop a service."""
+        client = mut.WindowsServiceClient(SAMPLE_MACHINE)
+        client.stop_service(service_name=SAMPLE_SERVICE)
+        m_Popen.assert_called_with([mut.SERVICE_CMD, SAMPLE_ADDR, mut.STOP_CMD, 
+                                    SAMPLE_SERVICE])
+        
--- src/clients/windows_service_client.py
+++ src/clients/windows_service_client.py
@@ -3,13 +3,31 @@ Created on 2011-11-21
 
 @author: Alec
 '''
+import subprocess
 
+SERVICE_CMD = "sc"
+START_CMD = "start"
+STOP_CMD = "stop"
+ADDR_TMPL = r"\\{0}"
 
 class WindowsServiceClient(object):
     
     
+    def __init__(self, machine):
+        self.machine = ADDR_TMPL.format(machine)
+    
+    
     def start_service(self, service_name):
-        pass
+        subprocess.Popen([SERVICE_CMD, self.machine, 
+                          START_CMD, service_name])
+        
+    def stop_service(self, service_name):
+        subprocess.Popen([SERVICE_CMD, self.machine, 
+                          STOP_CMD, service_name])
+        
+        
+if __name__ == "__main__":
+    WindowsServiceClient("arcvm").start_service("Alerter")
     
     
 
