b9198292659182e6742148224096dc25bf1bd419
Alec Munro <alec.munro@arc90.com>
2011-11-22 12:17:55 -0400

    Ignoring more things, and specifying nose as dependency.

--- setup.py
+++ setup.py
@@ -3,9 +3,26 @@ Created on 2011-11-21
 
 @author: Alec
 '''
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+#CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    "nose" #Testing
+    ]
+           
 
 from distutils.core import setup
 setup(name='MonSer',
       version='0.0.1',
+      long_description=README,
+      packages=find_packages(),
       py_modules=['clients'],
+      install_requires = requires,
+      tests_require = requires,
       )
